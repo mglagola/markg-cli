93c23331ad3813122eeeeaea3821ed76f59de36a
Mark Glagola <mark.glagola@gmail.com>
2017-11-25 10:13:56 -0600

    ramda + redux-request-maker (rm lodash) for nextjs template

--- templates/nextjs/reducers/Search.js
+++ templates/nextjs/reducers/Search.js
@@ -1,62 +1,13 @@
-import get from 'lodash/get';
-import omit from 'lodash/omit';
+import { createReduxRequest } from 'redux-request-maker';
+import xhr from '@utils/xhr';
 
-export const SEARCH_REQUEST = 'SEARCH_REQUEST';
-export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
-export const SEARCH_FAILURE = 'SEARCH_FAILURE';
-
-const fetchPredictions = () => ({ results: ['NOTHING!'] });
-
-export const searchGooglePlaces = ({ keyword }) => ({
-    types: [
-        SEARCH_REQUEST,
-        SEARCH_SUCCESS,
-        SEARCH_FAILURE,
-    ],
-    callAPI: () => fetchPredictions({ keyword }).then(results => get(results, 'predictions', [])),
-    payload: { keyword },
+const {
+    reducer,
+    request,
+} = createReduxRequest({
+    actionTypePrefix: 'search',
+    callAPI: async (query, state) => xhr.get('api/fake/endpoint', query),
 });
 
-const ACTION_HANDLERS = {
-    [SEARCH_REQUEST]: (state, action) => ({
-        ...omit(state, 'error'),
-        status: 'loading',
-        keyword: get(action, 'payload.input'),
-    }),
-    [SEARCH_SUCCESS]: (state, action) => {
-        const lastRequestedKeyword = get(state, 'keyword');
-        const keyword = get(action, 'payload.input');
-        if (lastRequestedKeyword !== keyword) {
-            return { ...state };
-        }
-        return {
-            ...state,
-            status: 'success',
-            results: get(action, 'response', []),
-        };
-    },
-    [SEARCH_FAILURE]: (state, action) => {
-        const lastRequestedKeyword = get(state, 'keyword');
-        const keyword = get(action, 'payload.input');
-        if (lastRequestedKeyword !== keyword) {
-            return { ...state };
-        }
-        return {
-            ...state,
-            status: 'failure',
-            error: get(action, 'error'),
-        };
-    },
-};
-
-export const defaultState: Object = {
-    keyword: undefined,
-    error: undefined,
-    results: undefined,
-    status: 'notasked',
-};
-
-export default function reducer (state: Object = defaultState, action: Object) {
-    const handler = ACTION_HANDLERS[action.type];
-    return handler ? handler(state, action) : state;
-}
+export request;
+export default reducer;
